2e49b6fe298d440dc5e11362d3f617ec4135b64e
angelicaroque29 <54654695+angelicaroque29@users.noreply.github.com>
2022-04-20 11:01:40 -0400

    Main Level

--- DaGame/Assets/Scripts/circleMotion.cs
+++ DaGame/Assets/Scripts/circleMotion.cs
@@ -5,23 +5,40 @@ using UnityEngine;
 public class circleMotion : MonoBehaviour
 {
 
-	float timeCounter = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject rotatedObject;
+    bool rotating = false;
+    public float smoothTime = 5.0f; //rotate over 5 seconds
+ 
+    void OnCollisionEnter(Collision col)
+     {
+        if (col.gameObject.name == "LeftWall" || col.gameObject.name == "RightWall" && !rotating) // we dont want to call this if the object is already rotating, so we check if it is
+         {
+            //Rotate rotatedObject by 90 degrees on the Y axis
+            rotating = true;
+            float rando = Random.Range(0, 100); // pick a random number between 1 and 100
+            int multiplier = 1;
+            if (rando > 50)
+             {
+                 multiplier = -1;
 
-    // Update is called once per frame
-    void Update()
-    {
-    	timeCounter += Time.deltaTime;
+             }
+             StartCoroutine(RotateOverTime(rotatedObject.transform.localEulerAngles.y, rotatedObject.transform.localEulerAngles.y + (180 * multiplier), smoothTime));
+            
+        }
+     }
+    IEnumerator RotateOverTime(float currentRotation, float desiredRotation, float overTime)
+     {
+         float i = 0.0f;
+         while (i <= 1)
+         {
+             rotatedObject.transform.localEulerAngles = new Vector3(rotatedObject.transform.localEulerAngles.x, Mathf.Lerp(currentRotation, desiredRotation, i), rotatedObject.transform.localEulerAngles.z);
+             i += Time.deltaTime / overTime;
+             yield return null;
+         
+         }
+         yield return new WaitForSeconds(overTime);
+         rotating = false; // no longer rotating
 
-    	float x = Mathf.Cos (timeCounter);
-    	float y = Mathf.Sin (timeCounter);
-    	float z = 0;
+     }
+ }
 
-    	transform.position = new Vector3(x,y,z);
-        
-    }
-}
